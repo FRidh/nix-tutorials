8af3d43a9706393aaed55f21f763086c7e14c3bf
Frederik Rietdijk <fridh@fridh.nl>
2020-03-16 20:45:39 +0100

    fetch nixpkgs tarball instead of via git for faster CI

--- default.nix
+++ default.nix
@@ -1,17 +1,25 @@
 # The environment that it used to execute the notebooks.
 let
-  nixpkgs = fetchGit {
-    url = https://github.com/NixOS/nixpkgs.git;
-    ref = "nixos-19.09";
+  fetchTarballFromGitHub = {repo, owner, rev, sha256}: fetchTarball {
+    url = "https://github.com/${owner}/${repo}/archive/${rev}.tar.gz";
+    inherit sha256;
+  };
+
+  nixpkgs = fetchTarballFromGitHub {
+    owner = "NixOS";
+    repo = "nixpkgs";
     rev = "64565f9d8ffe5bc3737ebd5f6b97756fac16d23b";
+    sha256 = "1n31xffqi1lfaf44k38z6bx7g4scj5nibx9zcnf06mqn4fzakgd6";
   };
 
   pkgs = import nixpkgs {};
 
   python = pkgs.python3;
 
-  pandoc = (import (fetchTarball {
-    url = "https://github.com/Mic92/nur-packages/archive/0c3d7e8545a8f01e72fb79d3f17137267e9c20dc.tar.gz";
+  pandoc = (import (fetchTarballFromGitHub {
+    owner = "Mic92";
+    repo = "nur-packages";
+    rev = "0c3d7e8545a8f01e72fb79d3f17137267e9c20dc";
     sha256 = "0c0dg4177kwmgfnpv15sbgd2x1l04bgddw9qnr8qsj4b8dpp945i";
   }) {inherit pkgs;}).pandoc-bin;  # Much smaller, faster CI
 

37723b6440c0a489eee1b26b859c86a6dcd93288
FRidh <freddyrietdijk@fridh.nl>
2020-03-17 12:34:05 +0000

    Deploying to gh-pages from  - dd8841affb994a95ab8cfe7cd48693a9c4db4dd9 🚀

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '315d7c1',
+    VERSION: 'dd8841a',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
